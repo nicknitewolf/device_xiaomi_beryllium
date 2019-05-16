730b172375028003df742bbc969f34b638be4034
Nicholas Lim <nicknitewolf@lineageos.org>
2019-05-17 03:02:28 +0800

    beryllium: Update product makefile
     * Define keystore hw
     * Include tzdata

--- omni_beryllium.mk
+++ omni_beryllium.mk
@@ -1,11 +1,11 @@
 #
-# Copyright 2018 The Android Open Source Project
+# Copyright (C) 2017 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,17 +14,21 @@
 # limitations under the License.
 #
 
-# Release name
-PRODUCT_RELEASE_NAME := beryllium
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
 
 $(call inherit-product, build/target/product/embedded.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+# Time Zone data for recovery
+PRODUCT_COPY_FILES += \
+    system/timezone/output_data/iana/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.keystore=sdm845
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := beryllium
 PRODUCT_NAME := omni_beryllium
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Pocophone F1
+PRODUCT_MODEL := POCO F1
 PRODUCT_MANUFACTURER := Xiaomi
