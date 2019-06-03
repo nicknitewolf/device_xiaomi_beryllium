c43afe3372722dc6b152742ca1925f636b9e3679
Nicholas Lim <nicknitewolf@lineageos.org>
2019-06-03 16:29:52 +0800

    beryllium: Exclude MTP
     * Remove it because kernel panics

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,6 +72,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_EXCLUDE_MTP := true
 TW_EXTRA_LANGUAGES := true
 TW_HAS_EDL_MODE := true
 TW_INCLUDE_CRYPTO := true
